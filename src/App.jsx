406280dc1634b61c466bd63c7cb0fae593aa47cb
Sosnovskyi <roman97039703@gmail.com>
2025-01-09 23:36:36 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+
+function App() {
+  return (
+    <Container>
+      <Typography variant="h4" component="h1" gutterBottom>
+        Online Store Repo
+      </Typography>
+    </Container>
+  );
+}
+
+export default App;
