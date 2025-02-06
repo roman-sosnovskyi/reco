a3463947ba435552cd645e34bf90dc007e90259d
Mogreyn <alv87@bk.ru>
2025-02-06 17:57:29 +0200

    feat(OS-1st_block): fix responsivefix

--- src/pages/Product/MainPage.tsx
+++ src/pages/Product/MainPage.tsx
@@ -1,8 +1,6 @@
 import { MainPageProps } from "./types/MainPage.types";
 import styles from "./MainPage.module.scss";
 import HeroSection from "@/libs/components/HeroSection/HeroSection";
-import RecoBg from "@/libs/components/RecoBg/RecoBg";
-import Button from "@/libs/components/Button/Button";
 
 const MainPage = ({ className }: MainPageProps) => {
   const combinedClass = className
@@ -10,29 +8,7 @@ const MainPage = ({ className }: MainPageProps) => {
     : styles.main_page;
   return (
     <div className={combinedClass}>
-      <HeroSection>
-        <RecoBg />
-        <div className={styles.text_field}>
-          <p className={styles.slogan}>
-            <span className={styles.highlight}>RECO</span> — це бренд створений{" "}
-            <br /> з любов’ю до волосся
-          </p>
-          <p className={styles.text}>
-            Кожен заслуговує на здорове, сильне та блискуче волосся <br />
-            <span className={styles.highlight}>RECO</span> — це інструмент, який
-            дарує нове життя вашому
-            <br /> волоссю
-          </p>
-        </div>
-        <div className={styles.buttonBar}>
-          <Button size="l" variant="primary">
-            ЗАМОВИТИ
-          </Button>
-          <Button size="l" variant="black">
-            ДІЗНАТИСЬ БІЛЬШЕ
-          </Button>
-        </div>
-      </HeroSection>
+      <HeroSection />
     </div>
   );
 };
