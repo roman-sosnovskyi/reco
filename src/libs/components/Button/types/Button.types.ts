671707013f2d0f33b47a02b4d347508dd7fd7ad5
Aleksandr Vilgelmov <110188202+Mogreyn@users.noreply.github.com>
2025-02-11 17:56:51 +0200

    Revert "Alt dev"

--- src/libs/components/Button/types/Button.types.ts
+++ src/libs/components/Button/types/Button.types.ts
@@ -1,18 +1,7 @@
 import React from "react";
-export type ButtonSize = "s" | "m" | "l" | "black1" | "black2" | "black3";
-export type ButtonState =
-  | "default"
-  | "hover"
-  | "focus"
-  | "disabled"
-  | "pressed";
-export type ButtonVariant = "primary" | "secondary" | "black";
-export interface ButtonProps {
-  size?: ButtonSize;
-  disabled?: boolean;
-  variant?: ButtonVariant;
-  state?: ButtonState;
-  onClick?: () => void;
+export type ButtonProps = {
   children: React.ReactNode;
-  icon?: string;
-}
+  onClick?: () => void;
+  disabled?: boolean;
+  className?: string;
+};
