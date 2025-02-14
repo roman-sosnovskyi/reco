39ae1b4a07103b0c8157f658c96598de324a5ca1
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-14 16:12:13 +0200

    feat: integrate FeedbackForm into FeedbackSection component

--- src/components/FeedbackSection/FeedbackSection.tsx
+++ src/components/FeedbackSection/FeedbackSection.tsx
@@ -1,5 +1,15 @@
+import FeedbackForm from "../FeedbackForm/FeedbackForm";
+
 const FeedbackSection = () => {
-  return <h2>Залиште свої дані, щоб отримати більше інформації</h2>;
+  return (
+    <div>
+      <h2>Залиште свої дані, щоб отримати більше інформації</h2>
+      <div>
+        <FeedbackForm />
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default FeedbackSection;
