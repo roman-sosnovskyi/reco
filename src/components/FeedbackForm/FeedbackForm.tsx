1f060b318d9d9dee4f60401e39589bdd9943e5ab
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-14 16:12:27 +0200

    feat: add FeedbackForm component with validation for name and phone number

---
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -0,0 +1,47 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import { FormInput } from "./types/FeedbackForm.types";
+
+const FeedbackForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<FormInput>();
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="name">Ім&#39;я</label>
+        <input
+          id="name"
+          {...register("name", { required: "Це поле обов'язкове" })}
+          type="text"
+        />
+        {errors.name && <p>{errors.name.message}</p>}
+      </div>
+      <div>
+        <label htmlFor="phoneNumber">Номер телефону</label>
+        <input
+          id="phoneNumber"
+          maxLength={9}
+          {...register("phoneNumber", {
+            required: "Це поле обов'язкове",
+            pattern: {
+              value: /^\d{9}$/,
+              message: "Невірний формат номеру телефону"
+            }
+          })}
+          type="tel"
+        />
+        {errors.phoneNumber && <p>{errors.phoneNumber.message}</p>}
+      </div>
+      <button type="submit">Надіслати</button>
+    </form>
+  );
+};
+
+export default FeedbackForm;
