e0ad9e41501c64dc51a63c1d8a8bf46d60d4b2b8
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-20 10:53:26 +0200

    feat: enhance FeedbackForm with new icons and responsive layout adjustments

--- src/components/FeedbackForm/FeedbackForm.tsx
+++ src/components/FeedbackForm/FeedbackForm.tsx
@@ -2,6 +2,7 @@ import { useForm, SubmitHandler } from "react-hook-form";
 import { FormInput } from "./types/FeedbackForm.types";
 
 import style from "./FeedbackForm.module.scss";
+import Icon from "../Icon/Icon";
 
 const FeedbackForm = () => {
   const {
@@ -20,6 +21,13 @@ const FeedbackForm = () => {
       <div className={style.feedbackInputContainer}>
         <label htmlFor="name" className={style.feedbackInputLabel}>
           Ім&#39;я
+          <Icon
+            name="icon-star"
+            color="yellow"
+            size={24}
+            width={24}
+            style={{ height: 24, minHeight: 1, minWidth: 1, marginLeft: 10 }}
+          />
         </label>
 
         <input
@@ -40,18 +48,41 @@ const FeedbackForm = () => {
       <div className={style.feedbackInputContainer}>
         <label htmlFor="phoneNumber" className={style.feedbackInputLabel}>
           Номер телефону
+          <Icon
+            name="icon-star"
+            color="yellow"
+            size={24}
+            width={24}
+            style={{ height: 24, minHeight: 1, minWidth: 1, marginLeft: 10 }}
+          />
         </label>
 
-        <input
-          id="phoneNumber"
-          maxLength={13}
-          {...register("phoneNumber", {
-            required: "Це поле обовʼязкове"
-          })}
-          type="tel"
-          className={style.feedbackInput}
-          placeholder="+380 __ ___ __ __"
-        />
+        <div className={style.phoneNumberInputContainer}>
+          <input
+            id="phoneNumber"
+            maxLength={13}
+            {...register("phoneNumber", {
+              required: "Це поле обовʼязкове"
+            })}
+            type="tel"
+            className={style.feedbackInput}
+            placeholder="+380 __ ___ __ __"
+          />
+          <Icon
+            name="icon-phone"
+            color="none"
+            stroke="#96989B"
+            size={24}
+            style={{
+              position: "absolute",
+              top: -6,
+              right: 12,
+              width: 24,
+              minHeight: 1,
+              minWidth: 1
+            }}
+          />
+        </div>
 
         {errors.phoneNumber && (
           <p className={style.inputError}>{errors.phoneNumber.message}</p>
