dd44e8d4581a53d8b8a15734e28507ac919c4bd5
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-23 14:54:50 +0200

    feat: add Vite image optimizer plugin for improved image handling

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ViteImageOptimizer({
+    png: {
+          quality: 90
+        }
+      }),],
   publicDir: "public",
   resolve: {
     alias: {
