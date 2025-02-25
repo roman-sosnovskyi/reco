c6d6f4b9f8735bacf42955c74767e3115f59bfce
Sosnovskyi <roman97039703@gmail.com>
2025-02-25 19:08:06 +0100

    style: added new width and height button

--- src/components/Button/types/Button.types.ts
+++ src/components/Button/types/Button.types.ts
@@ -1,4 +1,4 @@
-import React, { JSX } from "react";
+import React, { ReactElement } from "react";
 export type ButtonSize = "s" | "m" | "l" | "black1" | "black2" | "black3";
 export type ButtonState =
   | "default"
@@ -14,6 +14,6 @@ export interface ButtonProps {
   state?: ButtonState;
   onClick?: () => void;
   children: React.ReactNode;
-  icon?: JSX.Element;
+  icon?: ReactElement | string;
   className?: string;
 }
--- src/components/HeroButtons/HeroButtons.tsx
+++ src/components/HeroButtons/HeroButtons.tsx
@@ -1,13 +1,14 @@
 import Button from "../Button/Button";
 import styles from "./HeroButtons.module.scss";
+import ArrowIcon from "@/assets/button/arow_btn.svg";
 
 const HeroButtons = () => {
   return (
     <div className={styles.buttonBar}>
-      <Button size="m" variant="primary">
+      <Button size="m" variant="primary" icon={ArrowIcon}>
         ЗАМОВИТИ
       </Button>
-      <Button size="m" variant="black">
+      <Button size="l" variant="secondary">
         ДІЗНАТИСЬ БІЛЬШЕ
       </Button>
     </div>
