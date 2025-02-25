e4076ecd55623a1f50b8cbe9f0cd1b5480c9bda9
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-25 10:10:09 +0200

    fix: remove Vite image optimizer plugin from configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), ViteImageOptimizer({
-    png: {
-          quality: 90
-        }
-      }),],
+  plugins: [react()],
   publicDir: "public",
   resolve: {
     alias: {
