3e58a0f9103a1e6214338e3131460e3cc71ba97c
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-26 20:40:40 +0200

    feat: enhance FeaturesSection with HighlightText component for improved text emphasis

--- src/components/FeaturesSection/FeaturesSection.tsx
+++ src/components/FeaturesSection/FeaturesSection.tsx
@@ -5,6 +5,7 @@ import recoil1xWebp from "@/assets/images/sections/features/recoil-desc-1x.webp"
 import recoil2xPng from "@/assets/images/sections/features/recoil-desc-2x.png";
 import recoil2xWebp from "@/assets/images/sections/features/recoil-desc-2x.webp";
 import styles from "./FeaturesSection.module.scss";
+import HighlightText from "../HighLightText/HighLightText";
 
 const FeaturesSection = ({ className }: FeaturesSectionProps) => {
   const classes = classNames(styles.featuresSection, className);
@@ -14,13 +15,15 @@ const FeaturesSection = ({ className }: FeaturesSectionProps) => {
       <h2>Особливості продукту</h2>
 
       <div className={styles.featuresLeftSide}>
-        <article className={styles.featuresArticleLeft}>
-          <h3>Ефективність</h3>
-          <p className={styles.featuresArticleLeftText}>
-            <span>Ефективність</span>
-            навіть для волосся з 5 ступенем пошкодження
-          </p>
-        </article>
+        <div className={styles.featuresArticleContainer}>
+          <article className={styles.featuresArticleLeft}>
+            <h3>Ефективність</h3>
+            <p className={styles.featuresArticleLeftText}>
+              <HighlightText>Ефективність </HighlightText>
+              навіть для волосся з 5 ступенем пошкодження
+            </p>
+          </article>
+        </div>
 
         <a href="#about">Про нас</a>
       </div>
@@ -39,24 +42,29 @@ const FeaturesSection = ({ className }: FeaturesSectionProps) => {
         </picture>
       </div>
 
-      <div className={styles.featuresArticlesRight}>
-        <article className={styles.featuresArticleRightTop}>
-          <h3>Простота у використанні:</h3>
-          <p className={styles.featuresArticleRightTopText}>
-            Простота у використанні: ідеально підходить як{" "}
-            <span>для професійних майстрів</span>, так і
-            <span>для домашнього догляду</span>
-          </p>
-        </article>
+      <div className={styles.featuresRightSide}>
+        <div className={styles.featuresArticleContainer}>
+          <article className={styles.featuresArticleRightTop}>
+            <h3>Простота у використанні</h3>
+            <p className={styles.featuresArticleRightTopText}>
+              Простота у використанні: ідеально підходить як{" "}
+              <HighlightText>для професійних майстрів</HighlightText>, так і
+              <HighlightText> для домашнього догляду</HighlightText>
+            </p>
+          </article>
+        </div>
 
-        <article className={styles.featuresArticleRightBottom}>
-          <h3>Комплексний підхід:</h3>
-          <p className={styles.featuresArticleRightBottomText}>
-            Комплексний підхід: RECO не лише відновлює, але й забезпечує{" "}
-            <span>захист, зволоження та живлення</span> завдяки лінійці
-            продуктів
-          </p>
-        </article>
+        <div className={styles.featuresArticleContainer}>
+          <article className={styles.featuresArticleRightBottom}>
+            <h3>Комплексний підхід</h3>
+            <p className={styles.featuresArticleRightBottomText}>
+              Комплексний підхід: <HighlightText>RECO</HighlightText> не лише
+              відновлює, але й забезпечує{" "}
+              <HighlightText>захист, зволоження та живлення</HighlightText>{" "}
+              завдяки лінійці продуктів
+            </p>
+          </article>
+        </div>
       </div>
     </section>
   );
