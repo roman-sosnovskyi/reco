ed5892f42fad982e2e9c1291647f8376dfa9ff0e
ptrnk0 <vlad.ptrnko@gmail.com>
2025-02-26 09:13:34 +0200

    feat: add webp image support and improve image handling in FeaturesSection

--- src/components/FeaturesSection/FeaturesSection.tsx
+++ src/components/FeaturesSection/FeaturesSection.tsx
@@ -1,7 +1,9 @@
 import classNames from "classnames";
 import { FeaturesSectionProps } from "./types/FeaturesSection.types";
-import recoil1x from "@/assets/images/sections/features/recoil-desc-1x.png";
-import recoil2x from "@/assets/images/sections/features/recoil-desc-2x.png";
+import recoil1xPng from "@/assets/images/sections/features/recoil-desc-1x.png";
+import recoil1xWebp from "@/assets/images/sections/features/recoil-desc-1x.webp";
+import recoil2xPng from "@/assets/images/sections/features/recoil-desc-2x.png";
+import recoil2xWebp from "@/assets/images/sections/features/recoil-desc-2x.webp";
 import styles from "./FeaturesSection.module.scss";
 
 const FeaturesSection = ({ className }: FeaturesSectionProps) => {
@@ -11,10 +13,10 @@ const FeaturesSection = ({ className }: FeaturesSectionProps) => {
     <section className={classes}>
       <h2>Особливості продукту</h2>
 
-      <div>
+      <div className={styles.featuresLeftSide}>
         <article className={styles.featuresArticleLeft}>
           <h3>Ефективність</h3>
-          <p>
+          <p className={styles.featuresArticleLeftText}>
             <span>Ефективність</span>
             навіть для волосся з 5 ступенем пошкодження
           </p>
@@ -23,17 +25,24 @@ const FeaturesSection = ({ className }: FeaturesSectionProps) => {
         <a href="#about">Про нас</a>
       </div>
 
-      <div>
+      <div className={styles.featuresImgContainer}>
         <picture>
-          <source srcSet={`${recoil1x} 1x, ${recoil2x} 2x`} />
-          <img src={recoil1x} alt="recoil" />
+          <source
+            srcSet={`${recoil1xWebp} 1x, ${recoil2xWebp} 2x`}
+            type="image/webp"
+          />
+          <source
+            srcSet={`${recoil1xPng} 1x, ${recoil2xPng} 2x`}
+            type="image/png"
+          />
+          <img src={recoil1xPng} alt="recoil" className={styles.featuresImg} />
         </picture>
       </div>
 
       <div className={styles.featuresArticlesRight}>
         <article className={styles.featuresArticleRightTop}>
           <h3>Простота у використанні:</h3>
-          <p>
+          <p className={styles.featuresArticleRightTopText}>
             Простота у використанні: ідеально підходить як{" "}
             <span>для професійних майстрів</span>, так і
             <span>для домашнього догляду</span>
@@ -42,7 +51,7 @@ const FeaturesSection = ({ className }: FeaturesSectionProps) => {
 
         <article className={styles.featuresArticleRightBottom}>
           <h3>Комплексний підхід:</h3>
-          <p>
+          <p className={styles.featuresArticleRightBottomText}>
             Комплексний підхід: RECO не лише відновлює, але й забезпечує{" "}
             <span>захист, зволоження та живлення</span> завдяки лінійці
             продуктів
