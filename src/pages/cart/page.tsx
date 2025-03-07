d91d7a418e8b2a9381831d6bc4fd6a046d8c8252
Mogreyn <alv87@bk.ru>
2025-03-07 16:03:29 +0200

    refactor: update route paths for consistency and adjust layout styles across components

--- src/pages/cart/page.tsx
+++ src/pages/cart/page.tsx
@@ -2,16 +2,19 @@ import styles from "./CartPage.module.scss";
 import CartItems from "@/components/CartItems/CartItems";
 import CartSummary from "@/components/CartSummary/CartSummary";
 import HighlightText from "@/components/HighLightText/HighLightText";
+import "@/styles/index.scss";
 
 export default function CartPage() {
   return (
-    <main className={styles.cartPage}>
-      <h1>
-        <HighlightText>КОШИК</HighlightText>
-      </h1>
-      <div className={styles.cartContainer}>
-        <CartItems />
-        <CartSummary />
+    <main className="container">
+      <div className={styles.cartPage}>
+        <h1>
+          <HighlightText>КОШИК</HighlightText>
+        </h1>
+        <div className={styles.cartContainer}>
+          <CartItems />
+          <CartSummary />
+        </div>
       </div>
     </main>
   );
