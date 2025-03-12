08d20273d06a21de3b7619b3d4c57a76248d8dbc
Sosnovskyi <roman97039703@gmail.com>
2025-03-12 12:51:38 +0100

    style(button): test fixed response HeroButton

--- src/components/HeroButtons/HeroButtons.tsx
+++ src/components/HeroButtons/HeroButtons.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
 import Button from "../Button/Button";
 import Icon from "../Icon/Icon";
 import styles from "./HeroButtons.module.scss";
+
 const HeroButtons = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className={styles.buttonBar}>
-      <Button size="m" variant="primary" className={styles.button}>
+      <Button
+        size={isMobile ? "s" : "l"}
+        variant="primary"
+        className={styles.button}
+      >
         <div className={styles.iconContainer}>
           <Icon
             name="icon-arrow-up-right2"
@@ -16,8 +33,17 @@ const HeroButtons = () => {
         </div>
         <span className={styles.button__text}>ЗАМОВИТИ</span>
       </Button>
-      <Button size="l" variant="secondary">
-        ДІЗНАТИСЬ БІЛЬШЕ
+      <Button size={isMobile ? "s" : "l"} variant="secondary">
+        <div className={styles.secondСontainer}>
+          <Icon
+            name="icon-arrow-up-right2"
+            size={isMobile ? 16 : 30}
+            fill="white"
+            stroke="none"
+            className={styles.SecondIcon}
+          />
+        </div>
+        <span className={styles.moreInfoText}>ПРО НАС</span>
       </Button>
     </div>
   );
