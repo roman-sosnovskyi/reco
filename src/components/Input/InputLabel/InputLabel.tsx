7d75e05f75d287649e10de2acd9b47b127825413
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-15 12:54:38 +0200

    feat(InputLabel): create InputLabel component with SCSS styling and required indicator

---
+++ src/components/Input/InputLabel/InputLabel.tsx
@@ -0,0 +1,28 @@
+import Icon from "@/components/Icon/Icon";
+
+import styles from "./InputLabel.module.scss";
+import { InputLableProps } from "./types/InputLabel.types";
+
+const InputLabel = ({
+  htmlFor,
+  children,
+  required = false
+}: InputLableProps) => {
+  return (
+    <>
+      <label htmlFor={htmlFor} className={styles.inputLabel}>
+        {children}
+        {required && (
+          <Icon
+            className={styles.inputIconStar}
+            name="icon-star"
+            stroke="#fbc000"
+            size={20}
+          />
+        )}
+      </label>
+    </>
+  );
+};
+
+export default InputLabel;
