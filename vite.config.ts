f0342d2399ba13110620151a5f9150f9a43f3b15
Aleksandr Vilgelmov <110188202+Mogreyn@users.noreply.github.com>
2025-03-16 18:01:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/reco/", // 👈 Добавлено для GitHub Pages
   plugins: [react()],
   publicDir: "public",
   resolve: {
