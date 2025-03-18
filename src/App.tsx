2643742723f4db9fe52d312c6173b84ddd38d11b
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-18 11:38:05 +0200

    feat: implement lazy loading for routes in App component with Suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import MainLayout from "@/core/MainLayout";
-import MainPage from "@/pages/mainPage/MainPage";
-import AboutPage from "@/pages/about/page";
-import ContactPage from "./pages/contacts/page";
-import FeedbackPage from "./pages/feedback/page";
-import CartPage from "./pages/cart/page";
-import SummaryPage from "./pages/summary/page";
-import CatalogPage from "./pages/catalog/page";
-import NotFoundPage from "./pages/notFound/page.tsx";
+import { lazy, Suspense } from "react";
+import MainLayout from "@/core/MainLayout.tsx";
+
+const MainPage = lazy(() => import("@/pages/mainPage/MainPage.tsx"));
+const AboutPage = lazy(() => import("@/pages/about/page.tsx"));
+const ContactPage = lazy(() => import("@/pages/contacts/page.tsx"));
+const FeedbackPage = lazy(() => import("@/pages/feedback/page.tsx"));
+const CartPage = lazy(() => import("@/pages/cart/page.tsx"));
+const SummaryPage = lazy(() => import("@/pages/summary/page.tsx"));
+const CatalogPage = lazy(() => import("@/pages/catalog/page.tsx"));
+const NotFoundPage = lazy(() => import("@/pages/notFound/page.tsx"));
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<MainPage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="contacts" element={<ContactPage />} />
-          <Route path="feedback" element={<FeedbackPage />} />
-          <Route path="cart" element={<CartPage />} />
-          <Route path="summary" element={<SummaryPage />} />
-          <Route path="catalog" element={<CatalogPage />} />
-          <Route path="*" element={<NotFoundPage />} /> {/* Обработка 404 */}
-        </Route>
-      </Routes>
-    </Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<MainPage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="contacts" element={<ContactPage />} />
+            <Route path="feedback" element={<FeedbackPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="summary" element={<SummaryPage />} />
+            <Route path="catalog" element={<CatalogPage />} />
+            <Route path="*" element={<NotFoundPage />} /> {/* Обработка 404 */}
+          </Route>
+        </Routes>
+      </Router>
+    </Suspense>
   );
 }
 
