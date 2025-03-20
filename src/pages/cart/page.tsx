3160701148ffdf723f9d13d11cf1b8ed4a130c98
ptrnk0 <vlad.ptrnko@gmail.com>
2025-03-20 19:13:47 +0200

    feat: add background circles to CartPage for enhanced visual appeal

--- src/pages/cart/page.tsx
+++ src/pages/cart/page.tsx
@@ -1,3 +1,4 @@
+import BackgroundCircles from "@/components/BackgroundCircles/BackgroundCircles";
 import styles from "./CartPage.module.scss";
 import CartItems from "@/components/CartItems/CartItems";
 import CartSummary from "@/components/CartSummary/CartSummary";
@@ -6,16 +7,20 @@ import "@/styles/index.scss";
 
 export default function CartPage() {
   return (
-    <main className="container">
-      <div className={styles.cartPage}>
-        <h1>
-          <HighlightText>КОШИК</HighlightText>
-        </h1>
-        <div className={styles.cartContainer}>
-          <CartItems />
-          <CartSummary />
+    <main>
+      <section className="container">
+        <div className={styles.cartPage}>
+          <BackgroundCircles className={styles.backgroundCirclesTop} />
+          <h1>
+            <HighlightText>КОШИК</HighlightText>
+          </h1>
+          <div className={styles.cartContainer}>
+            <CartItems />
+            <CartSummary />
+          </div>
+          <BackgroundCircles className={styles.backgroundCirclesCenter} />
         </div>
-      </div>
+      </section>
     </main>
   );
 }
